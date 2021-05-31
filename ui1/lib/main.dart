dc94ed594b879b722b8246dc0e904c86915ae0e7
utsav <utsavporwal01@gmail.com>
2021-05-31 13:06:02 +0530

    title changed

--- ui1/lib/main.dart
+++ ui1/lib/main.dart
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: ' UI Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'UI demo page'),
     );
   }
 }
